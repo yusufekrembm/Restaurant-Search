442f7be140e939aeb622a6c273d52cab6c854c07
YUSUF EKREM ÜNLÜ <88238748+yusufekrembm@users.noreply.github.com>
2022-12-03 21:59:19 +0300

    Api deleted

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL:  'https://api.yelp.com/v3/businesses',
     headers:{
-        Authorization : 'Bearer SActMQnWzYjRawNeYj7k-t5saOK9EuRdPb1UkHpeoVfgyks7Z4jUQCjX5m9KEK4wBtYW5hJrWH6cHLVFL0UrbgplDvZzguFdJMIJfHlcuAD6GYJtkS-x5ybAqvlXY3Yx'
+        Authorization : ''
     }
 });
